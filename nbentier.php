bd8b89ee567a0aeb1a11158521db2978369d0a79
MAUFROID Michel Pierre <m.maufroid@ics-nice.com>
2022-04-07 10:17:16 +0200

    Ajout d'un 2eme commentaire

--- nbentier.php
+++ nbentier.php
@@ -3,7 +3,7 @@ $nombre_entier = 0;
 $nombre_entier = retour_entier($nombre_entier);
 echo "Le nombre entier est $nombre_entier\n";
 // comment 
-
+// comment 2 
 
 function retour_entier($nombre_entier)
 {
