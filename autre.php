1576b690700064392f24d8ef831860d3da616d49
MAUFROID Michel Pierre <m.maufroid@ics-nice.com>
2022-04-07 11:12:41 +0200

    Nouvelle branche

---
+++ autre.php
@@ -0,0 +1,7 @@
+<?php
+$nombre_entier = 0;
+$nombre_entier = retour_entier($nombre_entier);
+echo "Le nombre entier est $nombre_entier\n";
+// comment 
+// comment 2 
+// comment 3
--- nbentier.php
+++ nbentier.php
@@ -4,6 +4,7 @@ $nombre_entier = retour_entier($nombre_entier);
 echo "Le nombre entier est $nombre_entier\n";
 // comment 
 // comment 2 
+// comment 3
 
 function retour_entier($nombre_entier)
 {
